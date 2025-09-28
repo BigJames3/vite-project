dcb658488d7fbdd3553f4b6631f87324de612e8d
TRAORE NIGNAMAN N'GOLO DOKPORO <traorenignaman@outlook.fr>
2025-09-28 23:40:26 +0000

    Passing Arguments

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,80 @@
-function Son(props) {
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+
+function kwtop(kw) {
+  return kw * 1.34102;
+}
+
+function Car(props) {
   return (
-    <div style={{background: 'lightgreen'}}>
-      <h2>Son</h2>
-      <div>{props.children}</div>
-    </div>
+    <h2>I am a Car {props.brand}!</h2>
+  );
+}
+class Cars extends React.Component {
+  render() {
+    return <h2>I am a Car!</h2>;
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h4>Who lives in my Garage?</h4>
+      <Cars />
+      </div>
+    );
+  }
+}
+
+function App() {
+  const x = "myclass";
+  const hp = 2000+10;
+  const myobj = {name: "Ford", model: "Mustang", year: 1969};
+  const mystules = {
+        color: "blue", 
+        fontSize: "30px", 
+        border: "2px solid red",
+        backgroundColor: "yellow",
+        marrgin: "10px",
+        padding: "10px",
+        borderRadius: "10px",
+        textAlign: "center",
+        textTransform: "uppercase",
+      };
+      const xt = 5;
+      let y = "Apple";
+      if (xt < 10) {
+        y = "Orange";
+      } else {
+        y = "Banana";
+      }
+
+      const val = 5;
+      const shoot = () => {
+        alert("Great Shot!");
+      }
+      const shooter = (a) => {
+        alert(a);
+      }
+
+  return (
+    <> 
+     <button onClick={shoot}>Take the shot!</button>
+     <button onClick={() => shooter("Goal!")}>Take the shot!</button>
+      <Garage />
+      <Car  brand = "BMW"/>
+      <h1 className={x}>{x}Hello World *-</h1>
+      <p style={mystules}> It has {x} horsepower</p>
+      <p className="myclass">{y} -- </p>
+      <p className="myclass">{(val) < 41 ? (xt) : "Apple"}</p>
+      <h1> Bon jour</h1>
+      <p> It has {2000/5} horsepower</p>
+      <p> It has {hp} horsepower</p>
+      <p> It has {kwtop(2)} horsepower</p>
+      <h1>my name is {myobj.name} {myobj.model} and I {myobj.year} Year</h1>
+    </>
   );
 }
 
-function Daughter(props) {
-  const {brand, model} = props;
-  return (
-    <div style={{background: 'lightblue'}}>
-      <h2>Daughter</h2>
-      <div>{props.children}</div>
-    </div>
-  );
-}
-
-function Parent() {
-  return (
-    <div>
-      <h1>My two Children</h1>
-      <Son>
-        <p>
-          This was written in the Parent component,
-          but displayed as a part of the Son component
-        </p>
-      </Son>
-      <Daughter>
-        <p>
-          This was written in the Parent component,
-          but displayed as a part of the Daughter component
-        </p>
-      </Daughter>
-    </div>
-  );
-}
-
-export default Parent;
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Parent from './App.tsx'
+import App from './App.tsx'
 // import TheCar from './Vehicule.tsx';
 
 
 createRoot(document.getElementById('sandy')!).render(
   <StrictMode>
-    <Parent />
+    <App />
   </StrictMode>,
 )
 
